49cb0d974c328b205620d3d2c81182dbffa78160
Joshua Moore <mojoshua@sheridancollege.ca>
2024-10-08 14:56:29 -0400

    Added Steps 1 2 3

--- GroupProject/link-list2.c
+++ GroupProject/link-list2.c
@@ -1,47 +1,50 @@
 //Activity 1: include the link-list header file
 #include "link-list2.h"
- 
-int main () 
-{
-  ItemPtr startPtr; 	 	//Activity 2: creating list pointer
-  startPtr = NULL;		//Activity 3: initialising the start of the list   
-  int choice;
-  int id; char name[20]; float price;
-  menu ();
-  scanf ("%d", &choice);
-  while (choice != 5)
-  {
-    switch (choice) //Activity 4: Test your link list tasks
-    {
-       case 1: printf ("\nEnter Value for Item for Insertion: ");
-               scanf ("%d%19s%f", &id,name,&price);
-	       startPtr = addItem (startPtr, id,name,price);
-	       printList (startPtr);
-	       printf ("\n");
-	       break;
-       case 2: printf ("\nEnter ID for deletion : ");
-	       scanf ("%d", &id);
-	       startPtr = removeItem (startPtr, id);
-	       printList (startPtr);
-	       printf ("\n");
-               break;
-       case 3: printf ("\nEnter Item Number to View : ");
-	       scanf ("%d", &id);
-	       viewItem (startPtr, id);
-	       printf ("\n");
-	       break;
-       case 4: printList (startPtr);
-	       printf ("\n");
-	       break;
-       default:
-	       printf ("Invalid Option... Please Try Again \n");
-	       break;
+ int main() {
+    ItemPtr startPtr = NULL;
+    int choice;
+    char id[10], name[20];
+    float price;
+
+    menu();
+    scanf("%d", &choice);
+    clearInputBuffer();
+
+    while (choice != 5) {
+        switch (choice) {
+            case 1:
+                printf("Enter Part ID, Name, and Price: ");
+                fgets(id, 10, stdin);  // Read ID
+                id[strcspn(id, "\n")] = '\0';  // Remove newline
+                fgets(name, 20, stdin);  // Read Name
+                name[strcspn(name, "\n")] = '\0';  // Remove newline
+                scanf("%f", &price);  // Read Price
+                clearInputBuffer();  // Clear the buffer
+                startPtr = addItem(startPtr, id, name, price);
+                printList(startPtr);
+                break;
+            case 2:
+                printf("Enter Part ID to remove: ");
+                fgets(id, 10, stdin);
+                id[strcspn(id, "\n")] = '\0';
+                startPtr = removeItem(startPtr, id);
+                printList(startPtr);
+                break;
+            case 3:
+                printf("Enter Part ID to view: ");
+                fgets(id, 10, stdin);
+                id[strcspn(id, "\n")] = '\0';
+                viewItem(startPtr, id);
+                break;
+            case 4:
+                printList(startPtr);
+                break;
+            default:
+                printf("Invalid Option... Please Try Again\n");
+                break;
+        }
+        menu();
+        scanf("%d", &choice);
+        clearInputBuffer();
     }
-      
-    menu ();
-    scanf ("%d", &choice);
-  }
-  
-}// end of main
-
-
+}
--- GroupProject/link-list2.h
+++ GroupProject/link-list2.h
@@ -1,137 +1,136 @@
-// Header file containing link-list definitions & implementation methods
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Structure Definition should have at least 3 different types/pieces
+// Structure Definition with Alphanumeric ID
 struct item {
-  int itemId;
-  char itemName[20];
-  float itemPrice;
-  struct item *nextPtr;
+    char itemID[10];       // Unique Alphanumeric Identifier
+    char itemName[20];     // Part Name
+    float itemPrice;       // Part Price
+    struct item *nextPtr;
 };
 
-// Structure renaming
 typedef struct item Item;
 typedef struct item *ItemPtr;
 
-/*Function Prototypes*/
-
-ItemPtr makeItem(int id,char name[20],float price);
-ItemPtr addItem(ItemPtr sPtr, int id,char name[20],float price);
+/* Function Prototypes */
+ItemPtr makeItem(char id[10], char name[20], float price);
+ItemPtr addItem(ItemPtr sPtr, char id[10], char name[20], float price);
 void printList(ItemPtr sPtr);
-ItemPtr removeItem (ItemPtr sPtr, int id);
-void viewItem (ItemPtr sPtr, int id);
-void menu ();
- 
-/*******Function Definitions******/
-ItemPtr makeItem(int id,char name[20],float price)
-{
+ItemPtr removeItem(ItemPtr sPtr, char id[10]);
+void viewItem(ItemPtr sPtr, char id[10]);
+void menu();
+void clearInputBuffer();
+
+/******* Function Definitions ******/
+ItemPtr makeItem(char id[10], char name[20], float price) {
     ItemPtr newPtr = (ItemPtr)malloc(sizeof(Item));
-    if(newPtr == NULL)
-    {
-      puts("Memory Issues... Item not created");
-    }else{
-      newPtr->itemId = id;
-      strcpy(newPtr->itemName, name);
-      newPtr->itemPrice = price;
-      newPtr->nextPtr = NULL;
+    if (newPtr == NULL) {
+        puts("Memory allocation failed... Item not created");
+    } else {
+        strcpy(newPtr->itemID, id);
+        strcpy(newPtr->itemName, name);
+        newPtr->itemPrice = price;
+        newPtr->nextPtr = NULL;
     }
     return newPtr;
 }
 
-ItemPtr addItem(ItemPtr sPtr, int id,char name[20],float price)
-{
-  ItemPtr newPtr, previousPtr, currentPtr;
-  previousPtr = NULL;
-  currentPtr = sPtr;
-  newPtr = makeItem(id,name,price);
-  while(currentPtr != NULL && id > currentPtr->itemId){
-    previousPtr = currentPtr;
-    currentPtr = currentPtr->nextPtr;
-  }  
-  if(previousPtr == NULL){// inserting at the start-of-list
-    newPtr->nextPtr = sPtr;
-    sPtr = newPtr;  
-  }
-  else{  // inserting elsewhere in the list
-    previousPtr->nextPtr = newPtr;
-    newPtr->nextPtr = currentPtr;
-  }
-  return sPtr;
-}
+ItemPtr addItem(ItemPtr sPtr, char id[10], char name[20], float price) {
+    ItemPtr newPtr, previousPtr, currentPtr;
+    previousPtr = NULL;
+    currentPtr = sPtr;
+    newPtr = makeItem(id, name, price);
 
-void printList(ItemPtr sPtr)
-{
-  ItemPtr tempPtr = sPtr;
-  if(tempPtr == NULL){
-    puts("List is empty... Nothing to print");
-  }else{
-    puts("The Items in the list are: ");
-    
-    while(tempPtr != NULL){
-      printf("%d %s %f\n", tempPtr->itemId, tempPtr->itemName, tempPtr->itemPrice);
-      tempPtr = tempPtr->nextPtr;
+    // Insert in order by alphanumeric ID
+    while (currentPtr != NULL && strcmp(id, currentPtr->itemID) > 0) {
+        previousPtr = currentPtr;
+        currentPtr = currentPtr->nextPtr;
     }
-    puts("");
-  }
+    if (previousPtr == NULL) {  // Insert at start of list
+        newPtr->nextPtr = sPtr;
+        sPtr = newPtr;
+    } else {  // Insert in between or at the end
+        previousPtr->nextPtr = newPtr;
+        newPtr->nextPtr = currentPtr;
+    }
+    return sPtr;
 }
 
-ItemPtr removeItem (ItemPtr sPtr, int id)
-{
-   ItemPtr previousPtr = NULL, currentPtr = sPtr, tempPtr = NULL;
-   if(sPtr == NULL){
-      puts("List is empty... nothing to delete");
-      return NULL;
-   }
-   while(currentPtr != NULL && id != currentPtr->itemId){
-     previousPtr = currentPtr;
-     currentPtr = currentPtr->nextPtr;
-   }
-   if(currentPtr == NULL){ 	// value was not found
-     printf("ID %d was not found in the list\n", id);
-   }else if(previousPtr == NULL){ // value to be deleted is at the front of the list
-     tempPtr = sPtr;
-     printf("%d %s %f deleted...\n", tempPtr->itemId, tempPtr->itemName, tempPtr->itemPrice);
-     sPtr = sPtr->nextPtr;
-     free(tempPtr);
-   }else{	// value to be deleted is elsewhere in the list
-     tempPtr = currentPtr;
-     printf("%d %s %f deleted...\n", tempPtr->itemId, tempPtr->itemName, tempPtr->itemPrice);
-     previousPtr->nextPtr = currentPtr->nextPtr;
-     free(tempPtr);
-   }
-   return sPtr;
+void printList(ItemPtr sPtr) {
+    ItemPtr tempPtr = sPtr;
+    if (tempPtr == NULL) {
+        puts("List is empty... Nothing to print");
+    } else {
+        puts("Auto Shop Inventory:");
+        while (tempPtr != NULL) {
+            printf("ID: %s, Part: %s, Price: %.2f\n", tempPtr->itemID, tempPtr->itemName, tempPtr->itemPrice);
+            tempPtr = tempPtr->nextPtr;
+        }
+        puts("");
+    }
 }
 
+ItemPtr removeItem(ItemPtr sPtr, char id[10]) {
+    ItemPtr previousPtr = NULL, currentPtr = sPtr, tempPtr = NULL;
+    if (sPtr == NULL) {
+        puts("List is empty... nothing to delete");
+        return NULL;
+    }
 
-void viewItem (ItemPtr sPtr, int id){
-   ItemPtr currentPtr = sPtr;
-   int position = 0;
-   if(sPtr == NULL){
-      puts("List is empty... nothing to view");
-      return;
-   }
-   while(currentPtr != NULL && id != currentPtr->itemId){
-     currentPtr = currentPtr->nextPtr;
-     position++;
-   }
-   if(currentPtr == NULL){ 	// value was not found
-     printf("ID %d was not found in the list\n", id);
-   }else{ // value was found
-     printf("%d %s %f found at location %d\n", currentPtr->itemId, currentPtr->itemName, currentPtr->itemPrice, position+1);
-   }
-
+    // Search for item by ID
+    while (currentPtr != NULL && strcmp(id, currentPtr->itemID) != 0) {
+        previousPtr = currentPtr;
+        currentPtr = currentPtr->nextPtr;
+    }
+    if (currentPtr == NULL) {  // ID not found
+        printf("ID %s not found in the list\n", id);
+    } else if (previousPtr == NULL) {  // Item is at the front
+        tempPtr = sPtr;
+        printf("Part %s (%s) deleted...\n", tempPtr->itemName, tempPtr->itemID);
+        sPtr = sPtr->nextPtr;
+        free(tempPtr);
+    } else {  // Item is in the middle or end
+        tempPtr = currentPtr;
+        printf("Part %s (%s) deleted...\n", tempPtr->itemName, tempPtr->itemID);
+        previousPtr->nextPtr = currentPtr->nextPtr;
+        free(tempPtr);
+    }
+    return sPtr;
 }
 
-void menu () 
-{
-  printf ("\t1: Insert Item into Ordered List\n");
-  printf ("\t2: Remove Item from List\n");
-  printf ("\t3: View Item from List\n");
-  printf ("\t4: Printing the List\n");
-  printf ("\t5: Exit\n");
-  printf ("\tEnter Choice: ");
-} 
- 
+void viewItem(ItemPtr sPtr, char id[10]) {
+    ItemPtr currentPtr = sPtr;
+    int position = 0;
 
+    if (sPtr == NULL) {
+        puts("List is empty... nothing to view");
+        return;
+    }
+
+    // Search for the item by ID
+    while (currentPtr != NULL && strcmp(id, currentPtr->itemID) != 0) {
+        currentPtr = currentPtr->nextPtr;
+        position++;
+    }
+
+    if (currentPtr == NULL) {  // ID not found
+        printf("ID %s not found in the list\n", id);
+    } else {  // Item found
+        printf("Part %s (%s) is at position %d\n", currentPtr->itemName, currentPtr->itemID, position + 1);
+    }
+}
+
+void menu() {
+    printf("\t1: Insert Part into Ordered List\n");
+    printf("\t2: Remove Part from List\n");
+    printf("\t3: View Part in List\n");
+    printf("\t4: Print List of Parts\n");
+    printf("\t5: Exit\n");
+    printf("\tEnter Choice: ");
+}
+
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
